6cd23c9c96b8c4d1deffa5b1edd0585dcd0866d2
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-12-23 18:05:51 +0100

    modif card vocab

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
-
+// Scroll To Top
     
+    const btn = document.getElementById('scrollTop');
 
+    btn.addEventListener('click', () => window.scrollTo({
+    top: 400,
+    behavior: 'smooth',
+    }));
