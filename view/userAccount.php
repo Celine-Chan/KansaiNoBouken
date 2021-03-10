398ffc5010b520a05f9f6531e56549235746cda5
Celine_lhp5 <celine.louvel.76@outlook.fr>
2021-03-10 16:54:04 +0100

    maj 10/03

--- view/userAccount.php
+++ view/userAccount.php
@@ -24,30 +24,34 @@ require_once '../controllers/controller_userAccount.php';
     </div>
 
     <div class="container mt-2 text-center text-dark">
-        <h1>Mon compte</h1>
-        <p>Bienvenue <?php echo $_SESSION['users_pseudo'] ?></p>
+        <h1><img src="../assets/img/kitsune.svg" alt="kitsuneprofil" class="kitsuneProfil border border-dark rounded px-2 py-2"> Bienvenue <?php echo $_SESSION['users_pseudo'] ?></h1>
     </div>
 
-    <div class="container d-flex">
-        <img src="../assets/img/kitsune.svg" alt="kitsuneprofil" class="kitsuneProfil border border-dark rounded px-2 py-2">
-        <h2 class="userPseudo ms-3 text-dark"><?php echo $_SESSION['users_pseudo'] ?></h2>
-    </div>
+    <div class="container d-flex mt-5 justify-content-around">
+        <div class="card col-8 mt-5 row">
+            <div class="card-body">
+                <div class="container d-flex text-dark">
+                    <h2>Mes informations personnelles :</h2>
+                </div>
 
-    <div class="card container mt-5">
-        <div class="card-body">
-            <div class="container d-flex text-dark">
-                <h2>Mes informations personnelles :</h2>
-            </div>
-
-            <div class="container text-dark mt-5">
-                <p>Mon Nom : <?php echo $usersDetail['users_lastname'] ?> </p>
-                <p>Mon Prénom : <?php echo $usersDetail['users_firstname'] ?></p>
-                <p>Ma date de naissance : <?php echo $usersDetail['users_birthdate'] ?></p>
-                <p>Mon adresse mail : <?php echo $usersDetail['users_mail'] ?></p>
-                <p>Genre : <?php echo $usersDetail['users_gender'] ?></p>
-                <p>J'ai déjà voyagé au Japon ? <?php echo $usersDetail['users_japantrip'] == 0 ? 'oui' : 'non' ?></p>
+                <div class="container text-dark mt-5">
+                    <p>Mon Nom : <?php echo $usersDetail['users_lastname'] ?></p>
+                    <p>Mon Prénom : <?php echo $usersDetail['users_firstname'] ?></p>
+                    <p>Ma date de naissance : <?php echo $usersDetail['users_birthdate'] ?></p>
+                    <p>Mon adresse mail : <?php echo $usersDetail['users_mail'] ?></p>
+                    <p>Genre : <?php echo $usersDetail['users_gender'] ?></p>
+                    <p>J'ai déjà voyagé au Japon ? <?php echo $usersDetail['users_japantrip'] == 0 ? 'oui' : 'non' ?></p>
+                </div>
             </div>
         </div>
+        <div class="row ms-5 col-4">
+            <img src="../assets/img/momiji.svg" alt="momoji" class="momiji">
+        </div>
+    </div>
+
+    <div class="container commentUser mt-5 text-dark">
+        <h2 class="">Vos commentaires postés...</h2>
+        <p class="">Vous n'avez pas encore de commentaires postés sur le site !</p>
     </div>
 
 
